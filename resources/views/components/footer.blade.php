533ad73e065f08752e944e63914ce095270e0e0f
Diego Robles <diegorobles031204@gmail.com>
2025-06-10 11:46:46 +0200

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,8 +3,8 @@
         <p class="text-sm">&copy; {{ date('Y') }} EcoTrueque. Todos los derechos reservados.</p>
 
         <div class="flex text-black space-x-4 mt-2 sm:mt-0">
-            <a href="{{ route('aviso.legal') }}">Aviso Legal</a>
-            <a href="{{ route('politica.privacidad') }}">Política de Privacidad</a>
+            <a href="{{ route('aviso.legal') }}">Aviso Legal---</a>
+            <a href="{{ route('politica.privacidad') }}">Política de Privacidad---</a>
             <a href="{{ route('contacto') }}">Contacto</a>
 
         </div>
